374073ae025029f4c791e06db7c9476ba924a4ca
Caleb Katzenstein <caleb@calokat.me>
2025-10-15 19:28:53 -0700

    Remove camera / uvc code

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env sh
-g++ main.cc imu.cc i2c.cc -I/usr/local/include/opencv4 -o trackit -L/usr/local/lib -luvc -li2c -lopencv_core -lopencv_highgui -lopencv_stitching -lopencv_imgcodecs -lopencv_features2d
+g++ main.cc imu.cc i2c.cc -o trackit -L/usr/local/lib -li2c
